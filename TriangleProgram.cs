4285933145f124f0a6751b46b9cb6cec2a596f0a
Haider Zahoor <haider2090@gmail.com>
2019-10-05 12:19:02 +0500

    Lab Task 7
    Converted Lab Task 7 from C to C#.

---
+++ TriangleProgram.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Example_Proj
+{
+
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            Program read = new Program(); // Apparently C# can't call functions until there is a class to call the function to. We called a new class 'read' inside Main function
+
+            read.DrawUpwardTriangle();
+            read.DrawRectangle();
+            Console.WriteLine("\n");
+            read.DrawRectangle();
+            read.DrawDownwardTriangle();
+            Console.ReadKey();
+        }
+        void DrawDownwardTriangle()
+        {
+            Console.WriteLine("*******");
+            Console.WriteLine(" ***** ");
+            Console.WriteLine("  *** ");
+            Console.WriteLine("   *  ");
+        }
+        void DrawRectangle()
+        {
+            Console.WriteLine("_______");
+            Console.WriteLine("_______");
+            Console.WriteLine("_______");
+            Console.WriteLine("_______");
+            Console.WriteLine("_______");
+        }
+        void DrawUpwardTriangle()
+        {
+            Console.WriteLine("   * ");
+            Console.WriteLine("  *** ");
+            Console.WriteLine(" ***** ");
+            Console.WriteLine("*******");
+
+        }
+    }
+}
